62f6fa99aa4483f5accf445e6d2fb8d074788ca3
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2023-08-17 10:35:43 +1000

    leverage react bootstrap to create card, button and include images

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,16 +1,40 @@
 import React from "react";
+import { Card, Stack, Button, Image } from "react-bootstrap";
+import quotationImg from "../assets/quotation.png";
 
 function QuoteBox(props) {
   return (
-    <>
-      <div>
-        <h1>"{props.text}"</h1>
-        <p>- {props.author}</p>
-      </div>
-      <button id="new-quote" onClick={props.generateNewQuoteIndex}>
+    <Stack
+      gap={4}
+      className="d-flex align-items-center justify-content-center vh-100"
+    >
+      <Card className="p-2">
+        <Card.Body>
+          <Image
+            src={quotationImg}
+            alt="quotation"
+            style={{ width: "50px", opacity: ".7" }}
+          />
+          {/* <Card.Title>{props.text}</Card.Title> */}
+          <Card.Text>
+            <h3>{props.text}</h3>
+            <p>- {props.author}</p>
+          </Card.Text>
+          <Image
+            src={quotationImg}
+            alt="quotation-rotated"
+            style={{
+              width: "50px",
+              transform: "rotate(180deg)",
+              opacity: ".7",
+            }}
+          />
+        </Card.Body>
+      </Card>
+      <Button variant="primary" onClick={props.generateNewQuoteIndex}>
         New Quote
-      </button>
-    </>
+      </Button>
+    </Stack>
   );
 }
 
