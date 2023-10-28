578511226ffc21179a729342ecc64963f57b9401
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2023-10-28 17:09:10 +1100

    add box using mantine's <Paper /> component

--- components/QuoteBox.jsx
+++ components/QuoteBox.jsx
@@ -1,3 +1,16 @@
+import { useState } from "react";
+import { Container, Text, Paper, Button } from "@mantine/core";
+import quotes from "../data/quotes.json";
+
 export default function QuoteBox() {
-  return <>hello world</>;
+  return (
+    <Container>
+      <Paper shadow="sm" p="xl" m="xl">
+        <Text size="xl" fw={700}>
+          This is some quote
+        </Text>
+        <Text ta="right">- Henry Lin</Text>
+      </Paper>
+    </Container>
+  );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,8 @@
-import { Button, Group } from "@mantine/core";
 import QuoteBox from "../components/QuoteBox";
 
 export default function IndexPage() {
   return (
     <>
-      <Group mt={50} justify="center">
-        <Button size="xl">Hello World</Button>
-      </Group>
       <QuoteBox />
     </>
   );
