ac4843a97f2cb1c9ff4d05f010e452a80f48b667
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2023-10-28 16:21:07 +1100

    add quotebox placeholder component

---
+++ components/QuoteBox.jsx
@@ -0,0 +1,3 @@
+export default function QuoteBox() {
+  return <>hello world</>;
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,13 @@
 import { Button, Group } from "@mantine/core";
+import QuoteBox from "../components/QuoteBox";
 
 export default function IndexPage() {
   return (
-    <Group mt={50} justify="center">
-      <Button size="xl">Hello World</Button>
-    </Group>
+    <>
+      <Group mt={50} justify="center">
+        <Button size="xl">Hello World</Button>
+      </Group>
+      <QuoteBox />
+    </>
   );
 }
