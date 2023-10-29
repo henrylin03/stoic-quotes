90205b64ffb15db2fca32ad0e473761a028ef74f
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2023-10-30 09:06:38 +1100

    add hover tooltip for github button

--- components/GithubFooter/GithubFooter.jsx
+++ components/GithubFooter/GithubFooter.jsx
@@ -1,19 +1,21 @@
-import { ActionIcon } from "@mantine/core";
+import { Tooltip, ActionIcon } from "@mantine/core";
 import { IconBrandGithub } from "@tabler/icons-react";
 
 export default function GitHubFooter() {
   return (
-    <ActionIcon
-      component="a"
-      href="https://github.com/henrylin03/"
-      target="_blank"
-      variant="outline"
-      color="grey"
-      size="xl"
-      radius="xl"
-      aria-label="Link to GitHub"
-    >
-      <IconBrandGithub stroke={2.5} />
-    </ActionIcon>
+    <Tooltip label="Link to GitHub" color="gray" position="bottom" offset={11}>
+      <ActionIcon
+        component="a"
+        href="https://github.com/henrylin03/"
+        target="_blank"
+        variant="outline"
+        color="grey"
+        size="xl"
+        radius="xl"
+        aria-label="Link to GitHub"
+      >
+        <IconBrandGithub stroke={2.5} />
+      </ActionIcon>
+    </Tooltip>
   );
 }
