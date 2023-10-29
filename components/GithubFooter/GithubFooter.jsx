58d9e0693137826221fbf6be342836dabd393875
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2023-10-30 09:12:25 +1100

    update icon for github and remove tooltip (looks too messy)

--- components/GithubFooter/GithubFooter.jsx
+++ components/GithubFooter/GithubFooter.jsx
@@ -1,21 +1,19 @@
-import { Tooltip, ActionIcon } from "@mantine/core";
-import { IconBrandGithub } from "@tabler/icons-react";
+import { ActionIcon } from "@mantine/core";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
 export default function GitHubFooter() {
   return (
-    <Tooltip label="Link to GitHub" color="gray" position="bottom" offset={11}>
-      <ActionIcon
-        component="a"
-        href="https://github.com/henrylin03/"
-        target="_blank"
-        variant="outline"
-        color="grey"
-        size="xl"
-        radius="xl"
-        aria-label="Link to GitHub"
-      >
-        <IconBrandGithub stroke={2.5} />
-      </ActionIcon>
-    </Tooltip>
+    <ActionIcon
+      component="a"
+      href="https://github.com/henrylin03/"
+      target="_blank"
+      variant="subtle"
+      color="black"
+      size="xl"
+      radius="xl"
+      aria-label="Link to GitHub"
+    >
+      <GitHubLogoIcon style={{ width: "80%", height: "80%" }} />
+    </ActionIcon>
   );
 }
