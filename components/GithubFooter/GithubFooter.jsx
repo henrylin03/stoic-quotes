830d2e88e19a673fae333557e668a62e5d2d61be
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2023-10-30 08:49:53 +1100

    add placeholder footer

---
+++ components/GithubFooter/GithubFooter.jsx
@@ -0,0 +1,3 @@
+export default function GitHubFooter() {
+  return <>hello world</>;
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,12 @@
 import { BackgroundImage } from "@mantine/core";
 import QuoteGenerator from "../components/QuoteGenerator";
+import GithubFooter from "../components/GithubFooter/GithubFooter";
 
 export default function IndexPage() {
   return (
     <BackgroundImage src="./school-of-athens-bg.jpg">
       <QuoteGenerator />
+      <GithubFooter />
     </BackgroundImage>
   );
 }
