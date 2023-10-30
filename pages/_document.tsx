6812b9cbdd8ea295b2d5afe05296239dd8dd75b3
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2023-10-31 09:29:54 +1100

    add link tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,17 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import { ColorSchemeScript } from "@mantine/core";
 
-export default function Document() {
+function Document() {
   return (
     <Html lang="en">
       <Head>
         <ColorSchemeScript />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
@@ -14,3 +20,5 @@ export default function Document() {
     </Html>
   );
 }
+
+export default Document;
