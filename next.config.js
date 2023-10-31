8a4cf115902de722da74116068b52be6ded7743d
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2023-11-01 08:51:46 +1100

    attempt to fix images in public/ not showing

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  basePath: isProd ? "/stoic-quotes" : "",
+  assetPrefix: isProd ? "/stoic-quotes/" : "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
