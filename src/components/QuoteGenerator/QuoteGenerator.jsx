30778726a8c817bc9f2ca4cce67026fe5e8d169f
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2023-11-10 18:52:57 +1100

    implement stack and use of state to track the last five quote indices

--- src/components/QuoteGenerator/QuoteGenerator.jsx
+++ src/components/QuoteGenerator/QuoteGenerator.jsx
@@ -8,28 +8,27 @@ export default function QuoteGenerator() {
   const getRandomQuoteIndex = () => Math.floor(Math.random() * quotes.length);
 
   const [quoteIndex, setQuoteIndex] = useState(getRandomQuoteIndex());
+  const [quoteIndexHistory, setQuoteIndexHistory] = useState([]);
   const [textVisible, setTextVisible] = useState(true);
 
   const handleButtonClick = () => {
     const updateQuoteIndex = () => {
       let newQuoteIndex = getRandomQuoteIndex();
-      while (newQuoteIndex === quoteIndex) {
+      while (quoteIndexHistory.includes(newQuoteIndex)) {
         newQuoteIndex = getRandomQuoteIndex();
       }
       setQuoteIndex(newQuoteIndex);
+      setQuoteIndexHistory([newQuoteIndex, ...quoteIndexHistory.slice(0, 4)]);
     };
 
     setTextVisible(false);
     setTimeout(() => {
       updateQuoteIndex();
-      // fades text back in
       setTextVisible(true);
     }, 550);
   };
 
   const quote = quotes[quoteIndex];
-
-  //TODO: FEATURE #1 - IMPLEMENT FEATURE THAT PREVENTS SAME QUOTE FROM BEING SHOWN IN THE LAST 5
 
   return (
     <Stack align="center" gap="xl">
