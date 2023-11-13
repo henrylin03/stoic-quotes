aa8d83b690b6a20b42c23d982baf9f1db647b381
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2023-11-14 09:50:17 +1100

    unnest Flex component and add todo comments

--- src/components/QuoteGenerator/QuoteBox.jsx
+++ src/components/QuoteGenerator/QuoteBox.jsx
@@ -12,6 +12,10 @@ import {
 import { IconShare } from "@tabler/icons-react";
 import styles from "./QuoteBox.module.css";
 
+//TODO: add functionality so that when user clicks open the share button, modal pops up to different social media platforms (facebook, x (twitter), linkedin, messenger, wechat, whatsapp)
+//TODO: design what the modal will look like
+//TODO: design what is shared (should add link to bottom to the site itself)
+
 export default function QuoteBox({ quote, textVisible }) {
   return (
     <Card shadow="lg" w="55vw" h="55vh" radius="xl">
@@ -29,27 +33,25 @@ export default function QuoteBox({ quote, textVisible }) {
         >
           {(transitionStyles) => (
             <Flex
+              direction="column"
+              justify="center"
+              w="75%"
               style={{
-                width: "75%",
-                alignItems: "center",
-                justifyContent: "center",
                 ...transitionStyles,
               }}
             >
-              <Flex direction="column" justify="center">
-                <Title order={1} px="xl" mb="sm" className={styles.text}>
-                  {quote.text}
-                </Title>
-                <Text
-                  ta="right"
-                  px="xl"
-                  pt="sm"
-                  className={styles.text}
-                  mt="2rem"
-                >
-                  - {quote.author}
-                </Text>
-              </Flex>
+              <Title order={1} px="xl" mb="sm" className={styles.text}>
+                {quote.text}
+              </Title>
+              <Text
+                ta="right"
+                px="xl"
+                pt="sm"
+                className={styles.text}
+                mt="2rem"
+              >
+                - {quote.author}
+              </Text>
             </Flex>
           )}
         </Transition>
