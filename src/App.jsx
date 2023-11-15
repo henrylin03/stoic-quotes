40b082ec5acf5075f170a88163316b36af7319e4
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2023-11-16 00:24:09 +1100

    simplify code by using stack props instead of center component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "@mantine/core/styles.css";
-import { MantineProvider, BackgroundImage, Stack, Center } from "@mantine/core";
+import { MantineProvider, BackgroundImage, Stack } from "@mantine/core";
 import { theme } from "./theme";
 import QuoteGenerator from "./components/QuoteGenerator";
 import GithubFooter from "./components/GithubFooter";
@@ -7,14 +7,11 @@ import GithubFooter from "./components/GithubFooter";
 export default function App() {
   return (
     <MantineProvider theme={theme}>
-      {" "}
       <BackgroundImage src="./school-of-athens-bg.jpg">
-        <Center h="100vh">
-          <Stack align="center" gap="xl">
-            <QuoteGenerator />
-            <GithubFooter />
-          </Stack>
-        </Center>
+        <Stack align="center" justify="center" h="100vh" gap="xl">
+          <QuoteGenerator />
+          <GithubFooter />
+        </Stack>
       </BackgroundImage>
     </MantineProvider>
   );
