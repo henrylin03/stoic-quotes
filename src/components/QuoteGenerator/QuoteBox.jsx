49d541125444aceec16a70b9532d99f530bcb120
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2023-11-16 13:44:03 +1100

    Fix responsive design issues - fixes #7 (#26)
    * reduce nesting of quotebox
    
    * move css from props into module.css for quotebox
    
    * add media queries
    
    * add media queries and test on various devices in chrome devtools
    
    * add build files

--- src/components/QuoteGenerator/QuoteBox.jsx
+++ src/components/QuoteGenerator/QuoteBox.jsx
@@ -3,64 +3,51 @@ import styles from "./QuoteBox.module.css";
 
 export default function QuoteBox({ quote, textVisible }) {
   return (
-    <>
-      <Card
-        shadow="lg"
-        w="55vw"
-        h="55vh"
-        radius="xl"
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
+    <Card shadow="lg" radius="xl" className={styles.quoteBox}>
+      <Image
+        src="/quotation.png"
+        alt="quotation mark"
+        className={styles.quotationImage}
+      />
+      <Transition
+        mounted={textVisible}
+        transition="fade"
+        duration={550}
+        timingFunction="ease"
       >
-        <Image
-          src="/quotation.png"
-          alt="quotation mark"
-          className={styles.quotationImage}
-        />
-        <Transition
-          mounted={textVisible}
-          transition="fade"
-          duration={550}
-          timingFunction="ease"
-        >
-          {(transitionStyles) => (
-            <div
-              style={{
-                width: "75%",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                height: "100vh",
-                ...transitionStyles,
-              }}
-            >
-              <Flex direction="column" justify="center" h="150%">
-                <Title order={1} px="xl" mb="sm" className={styles.text}>
-                  {quote.text}
-                </Title>
-                <Text
-                  ta="right"
-                  px="xl"
-                  pt="sm"
-                  className={styles.text}
-                  mt="3rem"
-                >
-                  - {quote.author}
-                </Text>
-              </Flex>
-            </div>
-          )}
-        </Transition>
-        <Image
-          src="/quotation.png"
-          alt="quotation mark"
-          className={styles.quotationImageRotated}
-        />
-      </Card>
-    </>
+        {(transitionStyles) => (
+          <div
+            style={{
+              width: "75%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              height: "100vh",
+              ...transitionStyles,
+            }}
+          >
+            <Flex direction="column" justify="center" h="150%">
+              <Title order={1} px="xl" mb="sm" className={styles.quoteText}>
+                {quote.text}
+              </Title>
+              <Text
+                ta="right"
+                px="xl"
+                pt="sm"
+                className={styles.authorText}
+                mt="3rem"
+              >
+                - {quote.author}
+              </Text>
+            </Flex>
+          </div>
+        )}
+      </Transition>
+      <Image
+        src="/quotation.png"
+        alt="quotation mark"
+        className={styles.quotationImageRotated}
+      />
+    </Card>
   );
 }
